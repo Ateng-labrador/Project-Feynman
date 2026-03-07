8c279d2c70fda7271b629aa72cdf0875f03a544f
Ateng-labrador <arlantengga8@gmail.com>
2026-03-07 23:27:15 +0700

    feat: menambahkan model

--- project/kalkulus/models.py
+++ project/kalkulus/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()    
+    title = models.CharField(max_length=30, blank=True)
+    body = models.TextField()
+    email = models.EmailField(default='nama@tanya.com')
+    addres = models.CharField(max_length=200 ,blank=True)
 
     def __str__(self):
         return "{}".format(self.title)
--- project/kalkulus/views.py
+++ project/kalkulus/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+
 def index(request):
+    # queryset
+    posts = Post.objects.all()
     context = {
-        'judul' : 'Kalkulus - Ryman Project'
+        'judul' : 'Kalkulus - Ryman Project',
+        'Posts': posts,
+
     }
     return render(request, 'kalkulus/index.html', context)
