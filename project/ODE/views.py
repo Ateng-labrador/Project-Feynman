0c218c074d74d52280da961bec1c3c3ada777bcb
Ateng-labrador <arlantengga8@gmail.com>
2026-03-08 10:46:36 +0700

    feat: menambahkan model lagi

--- project/ODE/views.py
+++ project/ODE/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
 def index(request):
+    posts = Post.objects.all()
     context = {
-        'judul' : 'PDB - Ryman Project'
+        'judul' : 'PDB - Ryman Project',
+        'Posts' : posts
     }
     return render(request, 'ODE/index.html', context)
