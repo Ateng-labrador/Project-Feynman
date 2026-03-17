dfecaa6d11238d8cb8b51b80e5ab6890cca9dbc0
Ateng-labrador <arlantengga8@gmail.com>
2026-03-17 17:42:44 +0700

    feat: menambahkan logo

--- project/mywebsite/views.py
+++ project/mywebsite/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'judul' : 'Ryman Project',
+        'judul' : 'project ryman',
     }
-    return render(request, 'index.html', context)
+    return render(request, 'base.html', context)
